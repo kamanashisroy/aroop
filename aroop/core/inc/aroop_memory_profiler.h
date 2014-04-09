7f35601bf55b281a7dc12e309aff71b14d65bdbf
Kamanashis Roy <kamanashisroy+aroopcode@gmail.com>
2014-04-09 22:45:59 +0600

    We have added missing header file

---
+++ aroop/core/inc/aroop_memory_profiler.h
@@ -0,0 +1,18 @@
+
+#ifndef AROOP_MEMORY_PROFILER_DUMP_H_
+#define AROOP_MEMORY_PROFILER_DUMP_H_
+
+
+#ifndef AROOP_CONCATENATED_FILE
+#include "core/txt.h"
+#include "aroop_memory_profiler.h"
+#endif
+
+typedef struct {
+	void*cb_data;
+	int (*cb)(void*log_data, struct aroop_txt*content);
+}aroop_write_output_stream_t;
+
+int aroop_memory_profiler_dump(aroop_write_output_stream_t log);
+
+#endif // AROOP_MEMORY_PROFILER_DUMP_H_
